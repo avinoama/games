29828e97f15ce43c36493ade471833f7455bd641
avinoam <avinoama2@gmail.com>
2013-04-27 20:56:06 +0300

    another step in bulding board pieces

--- board_games/board_game_pieces/board_game_pieces.js
+++ board_games/board_game_pieces/board_game_pieces.js
@@ -13,13 +13,19 @@
           piece_type: piece_types[pieces[i].type]
         });
       }
+      $(".game-canvas").bind("click",function(e) {
+        piece_end_tap(e)
+        });
     },
     create_piece: function(params){
       
-      div = $("<div/>");
-      div.text(params.piece.label).addClass("piece").addClass(params.piece.type).addClass(params.piece.id);
-      $("#tile_"+params.tile_position).append(div);
-
+      piece = $("<div/>");
+      piece.text(params.piece.label).addClass("piece").addClass(params.piece.type).addClass(params.piece.id).attr("id",params.piece.id);
+      piece.bind("click",function(){
+        piece_tap(this)
+        });
+      $("#tile_"+params.tile_position).append(piece);
+      
     // add another piece to game pieces
     },
     show_piece: function(piece) {
@@ -27,7 +33,31 @@
     },
     load_piece_type: function(machine_name) {
       
-    }
+    },
+    current: null
   };
- 
+  function piece_tap(piece) {
+    current = Drupal.behaviors.boardGamePieces.current;
+    if(current != piece.id) {
+      mark_piece(piece);
+      setTimeout(function(){Drupal.behaviors.boardGamePieces.current = piece.id;},1000);
+    }
+  }
+  function mark_piece(piece) {
+    $(".piece").removeClass("selected");
+    $(piece).addClass("selected");
+
+  }
+  function piece_end_tap(e) {
+    if(Drupal.behaviors.boardGamePieces.current!=undefined) {
+      console.log("piece_end_tap");
+      console.log(e);
+      Drupal.behaviors.boardGamePieces.current = undefined;
+      $(".piece").removeClass("selected");
+    }
+  }
+  function request_move() {
+    
+    
+  }
 })(jQuery);
